5f68c4e2594f20ee1dcbf998944fba2b74d5e4aa
James Yang <james.y.yang@cargosmart.com>
2019-08-30 17:47:17 +0800

    add swiper component and add package

---
+++ components/search.vue
@@ -0,0 +1,104 @@
+<template>
+	<view class="content">
+		<view class="search-box">
+			<text v-if="prefixIcon" class="iconfont cell-icon" :class="prefixIcon"></text>
+			<view class="input-box">
+				<input type="text" v-model="keyword" :placeholder="placeholder" :placeholder-class="placeholderClass" @confirm="handleSearch()"
+				 confirm-type="search" />
+			</view>
+
+			<text v-if="suffixIcon" class="iconfont cell-icon" :class="suffixIcon"></text>
+			<view v-else class="search-btn" @tap="handleSearch()">搜索</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			prefixIcon: {
+				type: String,
+				default: "",
+			},
+			suffixIcon: {
+				type: String,
+				default: "",
+			},
+			placeholder: {
+				type: String,
+				default: "请输入搜索关键字"
+			},
+			placeholderClass: {
+				type: String,
+				default: ""
+			},
+			disabled: {
+				type: Boolean,
+				default: false,
+			}
+		},
+		data() {
+			return {
+				keyword: "",
+			};
+		},
+	};
+</script>
+
+<style lang="scss">
+	.content {
+		position: absolute;
+		width: 100%;
+		left: 0;
+		top: 0upx;
+		z-index: 9999;
+		// background-color: $uni-bg-color;
+
+		.cell-icon {}
+
+		.search-box {
+			margin: 0 20rpx;
+			font-size: 28upx;
+			color: $font-color-base;
+			// background: rgba(255, 255, 255, .6);
+			display: flex;
+			justify-content: space-between;
+
+			.cell-icon {
+				align-self: center;
+				width: 56upx;
+				max-height: 60upx;
+				font-size: 38upx;
+			}
+
+			.input-box {
+				flex: 1;
+				width: 100%;
+				height: 60upx;
+				font-size: 28upx;
+				background-color: rgb(242, 242, 242);
+				display: flex;
+				flex-shrink: 1;
+				align-items: center;
+				padding: 6upx 20upx;
+				border-radius: 20px;
+				input {
+					width: 100%;
+				}
+			}
+
+			.search-btn {
+				width: 120upx;
+				text-align: center;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				flex-shrink: 0;
+				font-size: $uni-font-size-base;
+				color: $uni-bg-color;
+				background: linear-gradient(to right, #ff9801, #ff570a);
+				border-radius: 60upx;
+			}
+		}
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,103 +1,146 @@
 <template>
 	<view class="content">
-		<Search></Search>
-		Index Page
+		<!-- <searchComponent></searchComponent> -->
+		<view class="content-box">
+			<view class="swiper-section">
+				<!-- 标题栏和状态栏占位符 -->
+				<view class="titleNview-placing"></view>
+				<!-- 背景色区域 -->
+				<view class="titleNview-background" :style="{backgroundColor: titleNViewBackground}"></view>
+
+				<uni-swiper-dot :info="carouselList" :current="swiperCurrent" :dots-styles="dotsStyles" mode="long">
+					<swiper class="carousel" circular autoplay duration="500" @change="handleSwiperChange">
+						<swiper-item v-for="(item, index) in carouselList" :key="index" class="carousel-item" @click="handleNavToDetailPage({title: '轮播广告'})">
+							<image :src="item.src" />
+						</swiper-item>
+					</swiper>
+				</uni-swiper-dot>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
-	import Search from './search.vue';
+	import searchComponent from "@/components/search.vue";
+	import {
+		uniSwiperDot
+	} from "@dcloudio/uni-ui"
 	export default {
 		components: {
-			Search,
+			searchComponent,
+			uniSwiperDot,
 		},
 		data() {
 			return {
-				title: 'Hello',
+				swiperCurrent: 0,
+				swiperLength: 0,
+				carouselList: [],
+				titleNViewBackground: '',
+
+				dotsStyles: {
+					backgroundColor: 'rgba(224, 224, 224, 0.5)',
+					border: '1px rgba(224, 224, 224, 0.3) solid',
+					color: '#fff',
+					selectedBackgroundColor: 'rgba(255, 255, 255, 0.9)',
+					selectedBorder: '1px rgba(255, 255, 255, 0.5) solid'
+				},
 			}
 		},
 		onLoad() {
-
+			this.initialData();
 		},
 		methods: {
-		}
+			initialData() {
+				// TODO: loading swiper data
+				const carouselList = Object.assign([], [{
+					src: '/static/temp/banner2.jpg',
+					background: 'rgb(205, 215, 218)',
+				}, {
+					src: '/static/temp/banner1.jpg',
+					background: 'rgb(203, 87, 60)',
+				}, {
+					src: '/static/temp/banner3.jpg',
+					background: 'rgb(183, 73, 69)',
+				}]);
+				this.titleNViewBackground = carouselList[0].background;
+				this.carouselList = carouselList;
+			},
+			handleSwiperChange(e) {
+				const index = e.detail.current;
+				this.swiperCurrent = index;
+				this.titleNViewBackground = this.carouselList[index].background;
+			},
+			handleNavToDetailPage(title) {
+				console.log(title);
+			}
+		},
+		computed: {},
 	}
 </script>
 
 <style lang="scss">
 	.content {
-		/* 		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center; */
-	}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50upx;
-	}
+		/* #ifdef */
+		.content-box {
+			.swiper-section {
+				padding: 0;
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
-
-	.search-box {
-		background-color: rgb(242, 242, 242);
-		padding: 15upx 2.5%;
-		display: flex;
-		justify-content: space-between;
-
-		.mSearch-input-box {
-			width: 100%;
-		}
-
-		.input-box {
-			width: 85%;
-			flex-shrink: 1;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-
-			input {
-				width: 100%;
-				height: 60upx;
-				font-size: 32upx;
-				border: 0;
-				border-radius: 60upx;
-				-webkit-appearance: none;
-				-moz-appearance: none;
-				appearance: none;
-				padding: 0 3%;
-				margin: 0;
-				background-color: #ffffff;
+				.titleNview-placing {
+					padding-top: 0;
+					height: 0;
+				}
 			}
 		}
-	}
 
-	.placeholder-class {
-		color: #9e9e9e;
-	}
+		/* #endif */
 
-	.search-box .search-btn {
-		width: 15%;
-		margin: 0 0 0 2%;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex-shrink: 0;
-		font-size: 28upx;
-		color: #fff;
-		background: linear-gradient(to right, #ff9801, #ff570a);
-		border-radius: 60upx;
+		.search-box {}
+
+		.content-box {
+			.swiper-section {
+				padding: 0;
+
+				.titleNview-placing {
+					height: var(--status-bar-height);
+					padding-top: 44px;
+					box-sizing: content-box;
+				}
+
+				.titleNview-background {
+					position: absolute;
+					top: 0;
+					left: 0;
+					width: 100%;
+					height: 426upx;
+					transition: .4s;
+				}
+
+				.carousel {
+					width: 100%;
+					height: 350upx;
+
+					.carousel-item {
+						width: 100%;
+						height: 100%;
+						padding: 0 28upx;
+						overflow: hidden;
+					}
+
+					image {
+						width: 100%;
+						height: 100%;
+						border-radius: 10upx;
+					}
+				}
+
+				.swiper-dots {
+					left: 45upx;
+					bottom: 40upx;
+				}
+			}
+		}
+
+
 	}
 </style>
